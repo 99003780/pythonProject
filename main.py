eb71af1692550b2ffccd6bec2520717ae3273081
99003780 <sai.kiran@ltts.com>
2021-03-11 14:09:15 +0530

    updated

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 celsius = int(input())
 
-
-def con
-    v(c):
-    fahrenheit = 9 / 5 * celsius + 32
-
-
-fahrenheit = 9 / 5 * celsius + 32
+def conv(c):
+    #fahrenheit = 9 / 5 * celsius + 32
+    return 9 / 5 * celsius + 32
 
 print(fahrenheit)
+h
